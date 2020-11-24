986bf5adc03a25c621991fd6cf7836bb35292b80
StuThEmp <semenshulga1@gmail.com>
2020-11-24 18:16:16 +0400

    lab 3

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,14 +1,15 @@
 ﻿#include "omp.h"
 #include "iostream"
 #include <stdio.h>
+#include <ctime>
 
 
 
 using namespace std;
 
-const int NMAX = 50;
+const int NMAX = 500000000;
 
-const int LIMIT = 1;
+const int MMAX = 10;
 
 void main()
 
@@ -17,63 +18,91 @@ void main()
 
 	int i, j;
 
-	float sum;
+
+	srand(time(0));
 
 	//Переменные для расчета времени
 	double start; 
 	double end;
 
 	//Масиив динамичский, потому что иначе будет Stack Overflow
-	int** a = new int* [NMAX];
 
-	for (i = 0; i < NMAX; i++)
 
-		a[i] = new int[NMAX];
+	int* A = new int [NMAX];
+	for (int i = 0; i < NMAX; i++) {
+		A[i] = 1 + rand() % 10;
+	}
+
+	int* B = new int [NMAX];
+	for (int i = 0; i < NMAX; i++) {
+		B[i] = 1 + rand() % 10;
+	}
+
+	int* C = new int [NMAX];
+	int* C2 = new int[NMAX];
+
+	
 
 	start = omp_get_wtime();
 
 	/*Начало параллельного фрагмента*/
 
-#pragma omp parallel shared(a) if (NMAX>LIMIT)
-
-	{
-
-#pragma omp for private(i,j)
-
-		for (i = 0; i < NMAX; i++)
-
-			for (j = 0; j < NMAX; j++)
-
-				a[i][j] = i + j;
-
-#pragma omp for private(i,j,sum)
-
-		for (i = 0; i < NMAX; i++)
-
-		{
-
-			sum = 0;
-
-			for (j = 0; j < NMAX; j++)
-
-				sum += a[i][j];
-
-			printf("Сумма элементов строки %d равна %f\n", i, sum);
-
+	int total = 0;
+	#pragma omp parallel shared(C) 
+	{ 
+		#pragma omp for private(i, j) reduction(+:total)
+		for (i = 0; i < NMAX; i++) 
+		{ 
+			if (A[i] > B[i])
+				C[i] = A[i];
+			else
+				C[i] = B[i];
+			total = total + C[i];
 		}
-
-	}
+	} 
 	
 /* Завершение параллельного фрагмента */
 
 	end = omp_get_wtime();
 
-	//cout << endl << sum << endl;
+	/*for (int i = 0; i < NMAX; i++) {
+		printf("%d|", C[i]);
+	}*/
+
+	printf("Сумма элементов маccива равна %d\n", total);
+
+	cout << "Время при параллельном использовании вычислительных ресурсов " << end - start << endl;
+
+	double start2;
+	double end2;
+	int sum = 0;
+
+	start2 = omp_get_wtime();
 
 	for (i = 0; i < NMAX; i++)
+	{
+		if (A[i] > B[i])
+			C2[i] = A[i];
+		else
+			C2[i] = B[i];
+		sum += C2[i];
+	}
 
-		delete[]a[i];
+	end2 = omp_get_wtime();
 
-	cout << "Время " << end - start << endl;
+	/*for (int i = 0; i < NMAX; i++) {
+		printf("%d|", C2[i]);
+	}*/
+
+	printf("Сумма элементов маccива равна %d\n", sum);
+
+	cout << "Время без параллельного использования вычислительных ресурсов " << end2 - start2 << endl;
+	
+
+	//cout << endl << sum << endl;
+
+
+
+	
 
 }
